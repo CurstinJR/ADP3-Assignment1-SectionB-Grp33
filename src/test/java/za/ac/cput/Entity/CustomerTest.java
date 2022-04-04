fd08404b46e92bbf06f4903138b93b0840f7fea0
CurstinJR <cjrose13@protonmail.com>
2022-04-04 15:37:10 +0200

    Object Identity Test
    Changes:
    	- create entity package
    	- add employee entity
    	- test employee for object identity
    
    Refer to GitHub Issues: #1

---
+++ src/test/java/za/ac/cput/Entity/CustomerTest.java
@@ -0,0 +1,40 @@
+package za.ac.cput.Entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest
+{
+    private Customer customerA;
+    private Customer customerB;
+
+    @BeforeEach
+    void setUp()
+    {
+        customerA = new Customer(1L,
+                "Quinn",
+                "Frost",
+                "quinn@email.com",
+                "quinn@123");
+        customerB = new Customer(2L,
+                "Curstin",
+                "Rose",
+                "curstin@email.com",
+                "curstin@123");
+    }
+
+    @Test
+    public void objectIdentityTest_shouldBeSameObjects()
+    {
+        customerB = customerA;
+        assertSame(customerA, customerB);
+    }
+
+    @Test
+    public void objectIdentityTest_shouldNotBeSameObjects()
+    {
+        assertNotSame(customerA, customerB);
+    }
+}
